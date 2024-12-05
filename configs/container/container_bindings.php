4decf307eb0276c564d07da4aad0efe56f218fda
Ivo <ivan.perovic@protonmail.com>
2024-12-05 21:16:04 +0100

    install webpack

--- configs/container/container_bindings.php
+++ configs/container/container_bindings.php
@@ -9,17 +9,27 @@ use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
 use Psr\Container\ContainerInterface;
 use Slim\Views\Twig;
+use Symfony\Bridge\Twig\Extension\AssetExtension;
+use Symfony\Component\Asset\Package;
+use Symfony\Component\Asset\Packages;
+use Symfony\Component\Asset\VersionStrategy\JsonManifestVersionStrategy;
+use Symfony\WebpackEncoreBundle\Asset\EntrypointLookup;
+use Symfony\WebpackEncoreBundle\Asset\EntrypointLookupCollection;
+use Symfony\WebpackEncoreBundle\Asset\TagRenderer;
+use Symfony\WebpackEncoreBundle\Twig\EntryFilesTwigExtension;
 
 use function DI\create;
 
 return [
     Config::class  => create(Config::class)->constructor(require CONFIG_PATH . '/app.php'),
-    Twig::class                   => function (Config $config) {
+    Twig::class                   => function (Config $config, ContainerInterface $container) {
         $twig = Twig::create(VIEW_PATH, [
             'cache'       => STORAGE_PATH . '/cache/templates',
             'auto_reload' => AppEnvironment::isDevelopment($config->get('app_environment')),
         ]);
 
+        $twig->addExtension(new EntryFilesTwigExtension($container));
+        $twig->addExtension(new AssetExtension($container->get('webpack_encore.packages')));
         return $twig;
     },
     EntityManager::class          => function (Config $config) {
@@ -32,5 +42,14 @@ return [
             DriverManager::getConnection($config->get('doctrine.connection'), $ormConfig),
             $ormConfig
         );
-    }
+    },
+
+    'webpack_encore.packages'     => fn() => new Packages(
+        new Package(new JsonManifestVersionStrategy(BUILD_PATH . '/manifest.json'))
+    ),
+    '_default' => fn() => new EntrypointLookup(BUILD_PATH . '/entrypoints.json'),
+    'webpack_encore.tag_renderer' => fn(ContainerInterface $container) => new TagRenderer(
+        new EntrypointLookupCollection($container),
+        $container->get('webpack_encore.packages')
+    ),
 ];
