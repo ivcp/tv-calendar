34006a818334656511e52f5e69871ebe736119dd
Ivo <ivan.perovic@protonmail.com>
2024-12-19 15:59:41 +0100

    fix date regex

--- configs/routes/web.php
+++ configs/routes/web.php
@@ -7,5 +7,5 @@ use Slim\App;
 
 return function (App $app) {
     $app->get('/', [CalendarController::class, 'index']);
-    $app->get('/{year:\b[0-9]{4}\b}-{month:\b[0-9]{2}\b}', [CalendarController::class, 'getMonth']);
+    $app->get('/{year:\b[0-9]{4}\b}-{month:\b0[1-9]|1[0-2]\b}', [CalendarController::class, 'getMonth']);
 };
