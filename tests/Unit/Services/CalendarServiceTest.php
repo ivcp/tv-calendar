2a8706149ab6549604c6803788b747112df8c53a
Ivo <ivan.perovic@protonmail.com>
2024-12-19 15:59:22 +0100

    add calendar service test

---
+++ tests/Unit/Services/CalendarServiceTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use App\Services\CalendarService;
+use App\Services\EpisodeService;
+use DateTime;
+use DateTimeZone;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+class CalendarServiceTest extends TestCase
+{
+
+    public static function monthProvider(): array
+    {
+        return [
+            'december 2024' => ['2024-12', 1, 'test show'],
+            'february 2025' => ['2025-02', 6, 'test new name'],
+            'december 1999' => ['1999-12', 1, 'test'],
+        ];
+    }
+
+    #[DataProvider('monthProvider')]
+    public function test_gets_schedule_for_current_month(
+        string $month,
+        int $date,
+        string $expectedShow,
+    ): void {
+
+
+        $episodeService = $this->createMock(EpisodeService::class);
+        $episodeService->expects($this->any())->method('getEpisodesForMonth')->willReturn([[
+            'id' => 1,
+            'showName' => $expectedShow,
+            'episodeName' => 'ep name',
+            'season' => 1,
+            'number' => 42,
+            'summary' => 'a short summary',
+            'type' => 'regular',
+            'airstamp' => new DateTime($month . "-$date", new DateTimeZone('UTC'))
+        ]]);
+
+        $scheduleService = new CalendarService($episodeService);
+        $schedule = $scheduleService->getSchedule($month);
+
+
+        $days = (new DateTime($month))->format('t');
+        $this->assertCount((int)$days, $schedule['popular']);
+        $this->assertSame($expectedShow, $schedule['popular'][$date][0]->showName);
+    }
+}
