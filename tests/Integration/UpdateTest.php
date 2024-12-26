782613e0e9b1759a104d7635b55015796e45da26
Ivo <ivan.perovic@protonmail.com>
2024-12-26 18:31:29 +0100

    add test insert shows and eps

--- tests/Integration/UpdateTest.php
+++ tests/Integration/UpdateTest.php
@@ -4,15 +4,26 @@ declare(strict_types=1);
 
 namespace Tests\Integration;
 
+use App\DataObjects\EpisodeData;
+use App\DataObjects\ShowData;
+use App\Entity\Episode;
+use App\Entity\Show;
 use App\Services\EpisodeService;
 use App\Services\ShowService;
 use App\Services\TvMazeService;
 use App\Services\UpdateService;
+use DateTime;
+use DI\Container;
 use Doctrine\ORM\EntityManager;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
+
+require __DIR__ . '/../../configs/path_constants.php';
 
 class UpdateTest extends TestCase
 {
+
+    private Container $container;
 
     public function setUp(): void
     {
@@ -20,28 +31,140 @@ class UpdateTest extends TestCase
         if ($result > 0) {
             exit('runing migrations failed' . PHP_EOL);
         }
+        $this->container = require CONFIG_PATH . '/container/container.php';
     }
 
-    public function test_update(): void
+
+
+    #[DataProvider('dataProvider')]
+    public function test_update($updatedShows, $shows, $episodes, $fromFile): void
     {
 
-        require __DIR__ . '/../../configs/path_constants.php';
-        $container = require CONFIG_PATH . '/container/container.php';
+        /** @var EntityManager $entityManager */
+        $entityManager = $this->container->get(EntityManager::class);
+        $tvMazeService = $this->createStub(TvMazeService::class);
+        $tvMazeService->method('getUpdatedShowIDs')->willReturn($updatedShows);
+        $tvMazeService->method('getShows')->willReturn($shows);
+        $tvMazeService->method('getEpisodes')->willReturn(...$episodes);
 
-        $entityManager = $container->get(EntityManager::class);
-        $tvMazeService = $this->createConfiguredStub(
-            TvMazeService::class,
-            [
-                'getUpdatedShows'     => ['foo'],
-            ]
-        );
         $updateService = new UpdateService(
             new ShowService($entityManager),
             new EpisodeService($entityManager),
-            $tvMazeService
+            $tvMazeService,
+            $entityManager
         );
 
         $updateService->run();
+
+        $shows = $entityManager->getRepository(Show::class)->findAll();
+        $episodes = $entityManager->getRepository(Episode::class)->findAll();
+        $this->assertSame('test show 1', $shows[0]->getName());
+        if (!$fromFile) {
+            $this->assertSame(2, count($shows));
+            $this->assertSame(4, count($episodes));
+        } else {
+            $this->assertSame(200, count($shows));
+            $this->assertSame(8000, count($episodes));
+        }
+    }
+
+    public static function dataProvider(): array
+    {
+        $episodesFromFile = file_get_contents(__DIR__ . '/episodes.json');
+        $eps = json_decode($episodesFromFile);
+
+        $testShowsSimple = [];
+        for ($i = 1; $i <= 2; $i++) {
+            $testShowsSimple[] = new ShowData(
+                tvMazeId: $i,
+                status: 'running',
+                weight: 100,
+                name: "test show $i",
+                summary: "test $i summary",
+                genres: ['Comedy', 'Drama'],
+            );
+        }
+
+        $testUdatedIds10 = range(1, 200);
+        $testShows10 = [];
+        for ($i = 1; $i <= 200; $i++) {
+            $testShows10[] = new ShowData(
+                tvMazeId: $i,
+                status: 'running',
+                weight: 100,
+                name: "test show $i",
+                summary: "test $i summary",
+            );
+        }
+
+        $testEpArraysFromFile = [];
+        for ($i = 0; $i < 200; $i++) {
+            $testEpArraysFromFile[] = array_map(function ($index, $episode) use ($i, $eps) {
+                $epId = count($eps) * $i + $index + 1;
+                return new EpisodeData(
+                    tvMazeShowId: $i + 1,
+                    tvMazeEpisodeId: $epId,
+                    episodeName: $episode->name,
+                    seasonNumber: $episode?->season,
+                    episodeNumber: $episode?->number,
+                    episodeSummary: $episode?->summary,
+                    type: $episode?->type,
+                    airstamp: $episode?->airstamp ? new DateTime($episode->airstamp) : null,
+                    runtime: $episode?->runtime,
+                    imageMedium: $episode?->image?->medium,
+                    imageOriginal: $episode?->image?->original
+                );
+            }, range(0, count($eps) - 1), $eps);
+        }
+
+
+
+        return [
+            'test simple' => [
+                [1, 2],
+                $testShowsSimple,
+                [
+                    [
+                        new EpisodeData(
+                            tvMazeShowId: 1,
+                            tvMazeEpisodeId: 1,
+                            episodeName: 'test S1E1',
+                            seasonNumber: 1,
+                            episodeNumber: 1,
+                            episodeSummary: 'summary ep 1',
+                            type: 'regular',
+                            airstamp: new DateTime('2013-06-25T02:00:00+00:00'),
+                            imageMedium: 'img.link',
+                            runtime: 60
+                        ),
+                        new EpisodeData(
+                            tvMazeShowId: 1,
+                            tvMazeEpisodeId: 2,
+                            episodeName: 'test S1E2'
+                        )
+                    ],
+                    [
+                        new EpisodeData(
+                            tvMazeShowId: 2,
+                            tvMazeEpisodeId: 3,
+                            episodeName: 'test S2E1'
+                        ),
+                        new EpisodeData(
+                            tvMazeShowId: 2,
+                            tvMazeEpisodeId: 4,
+                            episodeName: 'test S2E2'
+                        )
+                    ],
+                ],
+                false
+            ],
+            'test 10 shows, 5055 eps each' => [
+                $testUdatedIds10,
+                $testShows10,
+                $testEpArraysFromFile,
+                true
+            ],
+        ];
     }
 
 
