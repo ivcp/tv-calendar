8ed472e6d414f99232ecd9a60b94700193b40979
Ivo <ivan.perovic@protonmail.com>
2024-12-30 12:00:23 +0100

    add update shows

--- app/Services/UpdateService.php
+++ app/Services/UpdateService.php
@@ -19,29 +19,95 @@ class UpdateService
 
     public function run(): void
     {
-        echo 'MEMORY BEFORE ' .   memory_get_usage() . PHP_EOL;
+        echo 'UPDATE run() MEMORY USAGE START ' . memory_get_usage() . PHP_EOL;
 
         $updatedShowIDs = $this->tvMazeService->getUpdatedShowIDs();
         if (!$updatedShowIDs) {
             //log
+            echo 'NO SHOWS' . PHP_EOL;
             return;
         }
 
         //getallshows
         $updatedShowsData = $this->tvMazeService->getShows($updatedShowIDs);
 
-
         $showsInDB = $this->showService->getShowsByTvMazeId($updatedShowIDs);
 
         $showsInDBIds = $showsInDB ? array_map(fn($show) => $show->getTvMazeId(), $showsInDB) : [];
 
-        $showsToInsert = array_filter($updatedShowsData, fn($show) => !in_array($show->tvMazeId, $showsInDBIds));
+        $showsToInsert = array_values(
+            array_filter($updatedShowsData, fn($show) => !in_array($show->tvMazeId, $showsInDBIds))
+        );
 
+
+        if ($showsToInsert) {
+            $this->insertShowsAndEpisodes($showsToInsert);
+        }
+
+
+
+        $showsToUpdate = [];
+
+        foreach ($showsInDB as $show) {
+            $showData = null;
+            foreach ($updatedShowsData as $updatedData) {
+                if ($updatedData->tvMazeId === $show->getTvMazeId()) {
+                    $showData = $updatedData;
+                }
+            }
+            if ($showData) {
+                $showsToUpdate[$show->getId()] = $showData;
+            }
+        }
+
+
+        if ($showsToUpdate) {
+            try {
+                $updatedShows = $this->showService->updateShows($showsToUpdate);
+            } catch (\Throwable $e) {
+                //log
+                echo 'ERROR update shows: ' . $e->getMessage() . PHP_EOL;
+                return;
+            }
+
+            $epUpdatedCount = 0;
+
+            foreach ($showsToUpdate as $show) {
+                $episodes = $this->tvMazeService->getEpisodes($show->tvMazeId);
+
+                //insert eps //find by show_id
+            }
+
+
+            echo 'UPDATED SHOWS: ' . $updatedShows . PHP_EOL;
+        }
+
+
+
+
+
+
+        //TODO:
+        //update existing shows
+        //update eps
+
+
+        echo 'UPDATE run() MEMORY USAGE END ' . memory_get_usage() . PHP_EOL;
+
+
+        //clear memory or unset episodes
+        //implement logging
+
+    }
+
+    private function insertShowsAndEpisodes(array $showsToInsert): void
+    {
         try {
             $insertedShows = $this->showService->insertShows($showsToInsert);
         } catch (\Throwable $e) {
             //log
             //abort
+            echo 'ERROR insert shows: ' . $e->getMessage() . PHP_EOL;
             return;
         }
 
@@ -49,22 +115,20 @@ class UpdateService
 
         foreach ($showsToInsert as $show) {
             $episodes = $this->tvMazeService->getEpisodes($show->tvMazeId);
-            //bulk insert $episodes
-            //tryc
-            $insertedEpisodes = $this->episodeService->insertEpisodes($episodes);
-            $epInsertCount += $insertedEpisodes;
+
+            try {
+                $insertedEpisodes = $this->episodeService->insertEpisodes($episodes);
+                $epInsertCount += $insertedEpisodes;
+            } catch (\Throwable $e) {
+                //log it
+                echo 'ERROR insert episodes: ' . $e->getMessage() . PHP_EOL;
+            }
         }
+
+        $connected = $this->episodeService->connectEpisodesWithShows();
 
         echo 'SHOWS INSERTED: ' . $insertedShows . PHP_EOL;
         echo 'EPISODES INSERTED: ' . $epInsertCount . PHP_EOL;
-
-        echo 'MEMORY AFTER ' .   memory_get_usage() . PHP_EOL;
-
-
-        //clear memory or unset episodes
-
-
-
-
+        echo 'EPISODES CONNECTED: ' . $connected . PHP_EOL;
     }
 }
