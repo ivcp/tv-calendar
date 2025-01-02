162815f9e3779f40ddf5969f94021edef1870345
Ivo <ivan.perovic@protonmail.com>
2025-01-02 11:57:50 +0100

    add run update

--- scripts/update.php
+++ scripts/update.php
@@ -2,11 +2,8 @@
 
 declare(strict_types=1);
 
-use App\Services\ShowService;
-use App\Services\TvMazeService;
-use Doctrine\ORM\EntityManager;
+use App\Services\UpdateService;
 use Dotenv\Dotenv;
-use GuzzleHttp\Client;
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../configs/path_constants.php';
@@ -14,13 +11,7 @@ require __DIR__ . '/../configs/path_constants.php';
 $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
-$container      = require CONFIG_PATH . '/container/container.php';
+$container = require CONFIG_PATH . '/container/container.php';
 
-// $show = (new ShowService($container->get(EntityManager::class)))->getById(1);
-// var_dump($show->getSummary());
 
-$tvMazeService = new TvMazeService($container->get(Client::class));
-
-// $updated = $tvMazeService->getUpdatedShows();
-
-var_dump($tvMazeService->getShow(1));
+$container->get(UpdateService::class)->run();
