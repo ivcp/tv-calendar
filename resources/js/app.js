784efddaf440a5122f234a8e214c66df680d07ce
Ivo <ivan.perovic@protonmail.com>
2025-01-20 20:07:15 +0100

    dropdown closing with js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import "../css/app.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const dropdowns = document.querySelectorAll(".dropdown");
+
+  document.addEventListener("click", (e) => {
+    let target = false;
+    dropdowns.forEach((dropdown) => {
+      if (dropdown.contains(e.target)) {
+        target = true;
+      }
+    });
+
+    if (!target) {
+      dropdowns.forEach((d) => d.removeAttribute("open"));
+    }
+  });
+});
