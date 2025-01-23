80b3a2262c25e691335267ddd66b7aa83d968f8d
Ivo <ivan.perovic@protonmail.com>
2025-01-23 19:42:26 +0100

    add vaidation

--- app/Controllers/ShowController.php
+++ app/Controllers/ShowController.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Contracts\RequestValidatorFactoryInterface;
+use App\Enum\Genres;
+use App\Exception\BadRequestException;
+use App\RequestValidators\DiscoverRequestValidator;
 use App\Services\ShowService;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -13,34 +17,58 @@ class ShowController
 {
     public function __construct(
         private readonly Twig $twig,
-        private readonly ShowService $showService
+        private readonly ShowService $showService,
+        private readonly RequestValidatorFactoryInterface $requestValidatorFactory
     ) {
     }
 
     public function discover(Request $request, Response $response): Response
     {
-        $params = $request->getQueryParams();
         $start = 0;
         $length = 20;
-        if (isset($params['page']) && is_numeric($params['page'])) {
-            $page = (int) $params['page'];
-            if ($page > 0) {
-                $start = ($page - 1) * $length;
-            }
+        $page = 1;
+
+        $params = $this->requestValidatorFactory
+        ->make(DiscoverRequestValidator::class)
+        ->validate($request->getQueryParams());
+
+
+        if (isset($params['page'])) {
+            $pageNum = (int) $params['page'];
+            $start = ($pageNum - 1) * $length;
+            $page = $pageNum;
         }
 
         $sort = 'popular';
-        $sortOptions = ['popular', 'new'];
-        if (isset($params['sort']) && in_array($params['sort'], $sortOptions)) {
+
+        if (isset($params['sort'])) {
             $sort = $params['sort'];
         }
 
-        $shows =  $this->showService->getPaginatedShows($start, $length, $sort === 'new');
+        $totalPages =  ceil($this->showService->getShowCount() / $length);
+        if ($page > $totalPages) {
+            throw new BadRequestException();
+        }
 
+        $genre = Genres::Default->value;
+        if (isset($params['genre'])) {
+            $genre = $params['genre'];
+        }
 
-        //var_dump($shows);
-        //pass to twig
-        //query string to filter by genre, new, popular...
-        return $this->twig->render($response, 'shows/discover.twig', ['shows' => $shows]);
+        $shows =  $this->showService->getPaginatedShows($start, $length, $sort === 'new', $genre);
+
+        $pagination = ['page' => $page, 'totalPages' => $totalPages];
+        if (isset($params['sort'])) {
+            $pagination['sort'] = $sort;
+        }
+        if (isset($params['genre'])) {
+            $pagination['genre'] = $genre;
+        }
+
+        return $this->twig->render(
+            $response,
+            'shows/discover.twig',
+            ['shows' => $shows, 'pagination' => $pagination]
+        );
     }
 }
