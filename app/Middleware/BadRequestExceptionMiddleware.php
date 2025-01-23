e7330b42694907001d104f645747469bacdf1339
Ivo <ivan.perovic@protonmail.com>
2025-01-23 19:39:21 +0100

    add bad request exception

---
+++ app/Middleware/BadRequestExceptionMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware;
+
+use App\Contracts\SessionInterface;
+use App\Exception\BadRequestException;
+use App\Exception\ValidationException;
+use App\Services\RequestService;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Exception\HttpBadRequestException;
+
+class BadRequestExceptionMiddleware implements MiddlewareInterface
+{
+    public function __construct(
+        private readonly ResponseFactoryInterface $responseFactory,
+        private readonly SessionInterface $session,
+        private readonly RequestService $requestService,
+    ) {
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        try {
+            return $handler->handle($request);
+        } catch (BadRequestException $e) {
+            throw new HttpBadRequestException($request);
+        }
+    }
+
+}
