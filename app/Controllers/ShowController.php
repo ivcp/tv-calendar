44f72bd126a03daca35e5819e891e561afc1de15
Ivo <ivan.perovic@protonmail.com>
2025-01-24 19:51:18 +0100

    add store

--- app/Controllers/ShowController.php
+++ app/Controllers/ShowController.php
@@ -6,8 +6,11 @@ namespace App\Controllers;
 
 use App\Contracts\RequestValidatorFactoryInterface;
 use App\RequestValidators\DiscoverRequestValidator;
+use App\RequestValidators\StoreShowRequestValidator;
+use App\ResponseFormatter;
 use App\Services\PaginationService;
 use App\Services\ShowService;
+use App\Services\UserShowsService;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Views\Twig;
@@ -18,7 +21,9 @@ class ShowController
         private readonly Twig $twig,
         private readonly ShowService $showService,
         private readonly PaginationService $paginationService,
-        private readonly RequestValidatorFactoryInterface $requestValidatorFactory
+        private readonly RequestValidatorFactoryInterface $requestValidatorFactory,
+        private readonly ResponseFormatter $responseFormatter,
+        private readonly UserShowsService $userShowsService
     ) {
     }
 
@@ -30,10 +35,47 @@ class ShowController
 
         $discover = $this->paginationService->get($params);
 
+
+        $user = $request->getAttribute('user');
+        $userShows = [];
+        if ($user) {
+            $shows = $this->userShowsService->get($user);
+            $userShows = array_map(fn ($us) => $us->getShow()->getId(), $shows);
+        }
+
         return $this->twig->render(
             $response,
             'shows/discover.twig',
-            ['shows' => $discover->getShows(), 'pagination' => $discover->getPagination()]
+            [
+                'shows' => $discover->getShows(),
+                'pagination' => $discover->getPagination(),
+                'userShows' => $userShows,
+            ]
         );
+    }
+
+    public function store(Request $request, Response $response): Response
+    {
+        $params = $this->requestValidatorFactory
+        ->make(StoreShowRequestValidator::class)
+        ->validate($request->getParsedBody());
+
+        $user = $request->getAttribute('user');
+        if (! $user) {
+            return $this->responseFormatter->asJSON($response->withStatus(403), ['error' => 'log in to add shows']);
+        }
+        $showId = (int) $params['showId'];
+
+        $show = $this->showService->getById($showId);
+        if (! $show) {
+            return $this->responseFormatter->asJSON(
+                $response->withStatus(404),
+                ['error' => "show with id $showId does not exist"]
+            );
+        }
+
+        $this->userShowsService->add($show, $user);
+
+        return $this->responseFormatter->asJSON($response, ['success' => $show->getName() . ' added!']);
     }
 }
