ed72736bdb90f430e1331986e398965eae8ee484
Ivo <ivan.perovic@protonmail.com>
2025-01-25 19:43:02 +0100

    add delete show

--- resources/js/discover.js
+++ resources/js/discover.js
@@ -3,19 +3,38 @@ document.addEventListener("DOMContentLoaded", () => {
 
   addButtons.forEach((btn) =>
     btn.addEventListener("click", () =>
-      fetch("/showlist", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "X-Requested-With": "XMLHttpRequest",
-        },
-        body: JSON.stringify({
-          showId: btn.getAttribute("data-show-id"),
-          ...getCsrfFields(),
-        }),
-      })
+      btn.hasAttribute("added") ? del(btn) : post(btn)
     )
   );
+
+  async function post(el) {
+    const response = await fetch("/showlist", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+      },
+      body: JSON.stringify({
+        showId: el.getAttribute("data-show-id"),
+        ...getCsrfFields(),
+      }),
+    });
+  }
+
+  async function del(el) {
+    const showId = el.getAttribute("data-show-id");
+    const response = await fetch(`/showlist/${showId}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+      },
+      body: JSON.stringify({
+        _METHOD: "DELETE",
+        ...getCsrfFields(),
+      }),
+    });
+  }
 
   function getCsrfFields() {
     const csrfNameField = document.querySelector("#csrfName");
