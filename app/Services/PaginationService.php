e5505bd56bbf7d6ad2e06efa328573fc8435706d
Ivo <ivan.perovic@protonmail.com>
2025-01-26 21:50:27 +0100

    add showList

--- app/Services/PaginationService.php
+++ app/Services/PaginationService.php
@@ -4,22 +4,27 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use App\Entity\User;
 use App\Enum\Genres;
+use App\Enum\ShowListSort;
 use App\Enum\Sort;
 use App\Exception\NotFoundException;
+use Doctrine\ORM\NoResultException;
 
 class PaginationService
 {
     private int $start = 0;
     private int $length = 20;
     private int $page = 1;
-    private int $totalPages = 0;
-    private string $sort = Sort::Popular->value;
+    private int $totalPages = 1;
+    private string $sort;
     private string $genre = Genres::Default->value;
     private array $params;
+    private ?User $user = null;
 
     public function __construct(
-        private readonly ShowService $showService
+        private readonly ShowService $showService,
+        private readonly UserShowsService $userShowsService
     ) {
     }
 
@@ -29,7 +34,8 @@ class PaginationService
             $this->start,
             $this->length,
             $this->sort,
-            $this->genre
+            $this->genre,
+            $this->user
         );
     }
 
@@ -46,7 +52,7 @@ class PaginationService
         return $pagination;
     }
 
-    public function get(array $params): self
+    public function discover(array $params): self
     {
         if (isset($params['page'])) {
             $pageNum = (int) $params['page'];
@@ -54,11 +60,7 @@ class PaginationService
             $this->page = $pageNum;
         }
 
-        $this->totalPages = (int) ceil($this->showService->getShowCount() / $this->length);
-        if ($this->page > $this->totalPages) {
-            throw new NotFoundException();
-        }
-
+        $this->sort = Sort::Popular->value;
         if (isset($params['sort'])) {
             $this->sort = $params['sort'];
         }
@@ -67,7 +69,49 @@ class PaginationService
             $this->genre = $params['genre'];
         }
 
+        try {
+            $this->totalPages = (int) ceil($this->showService->getShowCount($this->genre) / $this->length);
+        } catch (NoResultException $e) {
+        }
+
+        if ($this->page > $this->totalPages) {
+            throw new NotFoundException();
+        }
+
         $this->params = $params;
+
+        return $this;
+    }
+
+    public function showList(array $params, User $user): self
+    {
+
+        if (isset($params['page'])) {
+            $pageNum = (int) $params['page'];
+            $this->start = ($pageNum - 1) * $this->length;
+            $this->page = $pageNum;
+        }
+
+        $this->sort = ShowListSort::Added->value;
+        if (isset($params['sort'])) {
+            $this->sort = $params['sort'];
+        }
+
+        if (isset($params['genre'])) {
+            $this->genre = $params['genre'];
+        }
+
+        try {
+            $this->totalPages = (int) ceil($this->userShowsService->getShowCount($user, $this->genre) / $this->length);
+        } catch (NoResultException $e) {
+        }
+
+        if ($this->page > $this->totalPages) {
+            throw new NotFoundException();
+        }
+
+        $this->params = $params;
+        $this->user = $user;
 
         return $this;
     }
