b59efe9ce66e321bc378d145bb667cabb8f141a4
Ivo <ivan.perovic@protonmail.com>
2025-01-26 21:45:48 +0100

    add showlist request validator

---
+++ app/RequestValidators/ShowListRequestValidator.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\RequestValidators;
+
+use App\Contracts\RequestValidatorInterface;
+use App\Enum\Genres;
+use App\Enum\ShowListSort;
+use App\Exception\BadRequestException;
+use Valitron\Validator;
+
+class ShowListRequestValidator implements RequestValidatorInterface
+{
+    public function validate(array $data): array
+    {
+
+        $v = new Validator($data);
+
+        $v->rules([
+            'optional' => [
+                ['page', 'sort', 'genre']
+            ],
+            'numeric' => [
+                ['page']
+            ],
+            'min' => [
+                ['page', 1]
+            ],
+            'in' => [
+                ['sort', array_column(ShowListSort::cases(), 'value')],
+                ['genre', array_column(Genres::cases(), 'value')]
+            ]
+        ]);
+
+        if (! $v->validate()) {
+            throw new BadRequestException();
+        }
+
+        return $data;
+    }
+
+}
