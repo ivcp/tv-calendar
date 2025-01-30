1d504d07b5251fb6af07e1a2f6862df943817626
Ivo <ivan.perovic@protonmail.com>
2025-01-30 11:40:01 +0100

    add update ui after delete

--- resources/js/showlist.js
+++ resources/js/showlist.js
@@ -1,7 +1,30 @@
 import { del } from "./ajax";
+import { ShowCount } from "./ShowCount";
 
 document.addEventListener("DOMContentLoaded", () => {
   const removeButtons = document.querySelectorAll(".remove-show");
+  const showCountElement = document.querySelector("[data-show-count]");
 
-  removeButtons.forEach((btn) => btn.addEventListener("click", () => del(btn)));
+  const initialShowCount = showCountElement.getAttribute("data-show-count");
+  if (!initialShowCount) {
+    console.error("no initial show count");
+    return;
+  }
+  const showCount = new ShowCount(+initialShowCount);
+
+  removeButtons.forEach((btn) =>
+    btn.addEventListener("click", async () => {
+      const result = await del(btn);
+      if (result.error) {
+        // notify err
+        console.log(result.messages);
+      }
+      showCount.decrement();
+      //check null
+      btn.parentElement.parentElement.remove();
+      showCountElement.textContent = `| ${showCount.showCount}`;
+      //notify
+      console.log(result.messages);
+    })
+  );
 });
