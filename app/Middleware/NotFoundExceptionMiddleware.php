5721099aaa08dcb7cb8ba0aec8976ee8b002fda4
Ivo <ivan.perovic@protonmail.com>
2025-02-01 19:48:44 +0100

    check for xhr

--- app/Middleware/NotFoundExceptionMiddleware.php
+++ app/Middleware/NotFoundExceptionMiddleware.php
@@ -8,6 +8,7 @@ use App\Contracts\SessionInterface;
 use App\Exception\BadRequestException;
 use App\Exception\NotFoundException;
 use App\Exception\ValidationException;
+use App\ResponseFormatter;
 use App\Services\RequestService;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -22,6 +23,7 @@ class NotFoundExceptionMiddleware implements MiddlewareInterface
         private readonly ResponseFactoryInterface $responseFactory,
         private readonly SessionInterface $session,
         private readonly RequestService $requestService,
+        private readonly ResponseFormatter $responseFormatter
     ) {
     }
 
@@ -30,6 +32,12 @@ class NotFoundExceptionMiddleware implements MiddlewareInterface
         try {
             return $handler->handle($request);
         } catch (NotFoundException $e) {
+
+            $response = $this->responseFactory->createResponse();
+            if ($this->requestService->isXhr($request)) {
+                return $this->responseFormatter->asJSONMessage($response, 404, 'page not found');
+            }
+
             throw new HttpNotFoundException($request);
         }
     }
