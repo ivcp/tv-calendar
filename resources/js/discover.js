56c91eac1c15db3327d5c1c27ab3262aa439a0b3
Ivo <ivan.perovic@protonmail.com>
2025-02-02 12:29:27 +0100

    change notifications

--- resources/js/discover.js
+++ resources/js/discover.js
@@ -1,11 +1,31 @@
 import { post, del } from "./ajax";
+import { notification } from "./notification";
 
 document.addEventListener("DOMContentLoaded", () => {
   const addButtons = document.querySelectorAll(".add-show");
 
   addButtons.forEach((btn) =>
-    btn.addEventListener("click", () =>
-      btn.hasAttribute("added") ? del(btn) : post(btn)
-    )
+    btn.addEventListener("click", async () => {
+      if (!btn.hasAttribute("added")) {
+        const result = await post(btn);
+        if (result.error) {
+          notification(result.messages, "alert-error");
+          return;
+        }
+        notification(result.messages, "alert-success");
+        btn.setAttribute("added", "");
+        btn.querySelector("svg").classList.add("fill-secondary");
+        return;
+      }
+
+      const result = await del(btn);
+      if (result.error) {
+        notification(result.messages, "alert-error");
+        return;
+      }
+      notification(result.messages, "alert-info");
+      btn.removeAttribute("added");
+      btn.querySelector("svg").classList.remove("fill-secondary");
+    })
   );
 });
