ede32d436c23207cec9f30fc31aa50e17ea4acf4
Ivo <ivan.perovic@protonmail.com>
2025-02-03 20:44:57 +0100

    add img path

--- configs/path_constants.php
+++ configs/path_constants.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 const APP_PATH     = __DIR__ . '/../app';
 const VIEW_PATH    = __DIR__ . '/../resources/views';
+const IMAGES_PATH    = __DIR__ . '/../resources/images';
 const STORAGE_PATH = __DIR__ . '/../storage';
 const CONFIG_PATH  = __DIR__ . '/../configs';
 const BUILD_PATH   = __DIR__ . '/../public/build';
