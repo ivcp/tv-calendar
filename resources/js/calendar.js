dd066acd2906f9c6f9ec92b95a69dc0388dda1fe
Ivo <ivan.perovic@protonmail.com>
2025-02-07 17:46:05 +0100

    add toggle user shows

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -1,22 +1,55 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const popularShowsBtn = document.querySelector("#popular-shows");
+  const userShowsBtn = document.querySelector("#user-shows");
+
   const scheduleData = document
     .querySelector("#calendar")
     .getAttribute("data-schedule");
   const data = JSON.parse(scheduleData);
 
-  for (const [key, value] of Object.entries(data.popular)) {
-    const cardElement = document
+  const activeClasses = ["tab-active", "bg-primary", "text-primary-content"];
+
+  const activateUserBtn = () => {
+    popularShowsBtn.classList.remove(...activeClasses);
+    userShowsBtn.classList.add(...activeClasses);
+  };
+  const activatePopularBtn = () => {
+    userShowsBtn.classList.remove(...activeClasses);
+    popularShowsBtn.classList.add(...activeClasses);
+  };
+
+  if (userShowsBtn.hasAttribute("active")) {
+    populateDates(data.user_shows);
+    activateUserBtn();
+  } else {
+    populateDates(data.popular);
+    activatePopularBtn();
+  }
+  popularShowsBtn.addEventListener("click", () => {
+    populateDates(data.popular);
+    activatePopularBtn();
+  });
+  userShowsBtn.addEventListener("click", () => {
+    populateDates(data.user_shows);
+    activateUserBtn();
+  });
+});
+
+function populateDates(shows) {
+  document.querySelectorAll(".card-body").forEach((e) => e.replaceChildren());
+  for (const [key, value] of Object.entries(shows)) {
+    const cardBody = document
       .querySelector(`#date-${key}`)
       .querySelector(".card-body");
-    value.forEach((show, i) =>
-      cardElement.insertAdjacentHTML(
+    value.forEach((show, i) => {
+      cardBody.insertAdjacentHTML(
         "beforeend",
         `<div class="bg-base-100 rounded-sm p-4 lg:p-2 lg:px-2 flex justify-between overflow-hidden">
         ${show.showName}<span class="">${show.seasonNumber}x${
           show.episodeNumber ?? "S"
         }</span>
         </div>`
-      )
-    );
+      );
+    });
   }
-});
+}
