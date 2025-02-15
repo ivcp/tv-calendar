799fc60cae828113989e22389272f7ca31b43f2d
Ivo <ivan.perovic@protonmail.com>
2025-02-15 16:18:09 +0100

    style search

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,9 @@ import "../css/app.css";
 
 document.addEventListener("DOMContentLoaded", () => {
   const dropdowns = document.querySelectorAll(".dropdown");
+  const searchBtn = document.querySelector("#search-btn");
+  const searchDiv = document.querySelector("#search");
+  const searchBackdrop = document.querySelector("#search-backdrop");
 
   document.addEventListener("click", (e) => {
     let target = false;
@@ -14,5 +17,17 @@ document.addEventListener("DOMContentLoaded", () => {
     if (!target) {
       dropdowns.forEach((d) => d.removeAttribute("open"));
     }
+  });
+
+  searchBtn.addEventListener("click", () => {
+    searchDiv.classList.remove("hidden");
+    searchDiv.classList.add("flex");
+    searchBackdrop.classList.remove("hidden");
+  });
+
+  searchBackdrop.addEventListener("click", () => {
+    searchDiv.classList.add("hidden");
+    searchDiv.classList.remove("flex");
+    searchBackdrop.classList.add("hidden");
   });
 });
