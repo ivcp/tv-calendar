34c8c83befe529def204486a842335e0d64eeb57
Ivo <ivan.perovic@protonmail.com>
2025-02-18 17:23:57 +0100

    edit weight

--- configs/app.php
+++ configs/app.php
@@ -39,5 +39,5 @@ return [
         'samesite' => 'lax',
     ],
     'json_tags' => JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_QUOT | JSON_HEX_APOS | JSON_THROW_ON_ERROR,
-    'popular_weight' => 100
+    'popular_weight' => 99
 ];
