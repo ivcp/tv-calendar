9a150d40c2837fc73bab707e7294e7dbe66719cb
Ivo <ivan.perovic@protonmail.com>
2025-03-25 14:06:48 +0100

    db backup

--- backupDB.sh
+++ backupDB.sh
@@ -6,9 +6,9 @@ if [ -z "$ORACLE_PAR" ]; then
     exit
 fi
 
-docker exec -it tv-calendar-db bash -c "pg_dump -U user -h localhost tv-calendar | gzip > backup.sql.gz"
-docker cp tv-calendar-db:backup.sql.gz .
-docker exec -it tv-calendar-db bash -c "rm backup.sql.gz"
+sudo docker exec -it tv-calendar-db bash -c "pg_dump -U user -h localhost tv-calendar | gzip > backup.sql.gz"
+sudo docker cp tv-calendar-db:backup.sql.gz .
+sudo docker exec -it tv-calendar-db bash -c "rm backup.sql.gz"
 
 curl -T backup.sql.gz "${ORACLE_PAR}backup.sql.gz"
 
