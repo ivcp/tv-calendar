a559a1b83c4e96e3a094407290f400b90ffbd986
Ivo <ivan.perovic@protonmail.com>
2025-12-03 13:32:58 +0100

    rate limit by cf connecting ip

--- get_cloudflare_ips.sh
+++ get_cloudflare_ips.sh
@@ -14,15 +14,6 @@ done
 echo "# Generated at $(LC_ALL=C date)"
 }
 
-set_real() {
-for type in v4 v6; do
-echo "# IP$type"
-curl -sL "https://www.cloudflare.com/ips-$type/" | sed "s|^|set_real_ip_from |g" | sed "s|\$|;|g"
-echo
-done
-
-echo "# Generated at $(LC_ALL=C date)"
-}
-
 (cf_ips && echo "deny all; # deny all remaining ips") > ./docker/nginx/allow-cloudflare-only.conf
-set_real > ./docker/nginx/set-real-ips.conf
+
+#
