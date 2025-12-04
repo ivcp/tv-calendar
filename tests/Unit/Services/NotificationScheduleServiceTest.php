6243ca4299b0ffdab1a7434f51e1247b40d2daad
Ivo <ivan.perovic@protonmail.com>
2025-12-04 16:29:44 +0100

    adjust time

--- tests/Unit/Services/NotificationScheduleServiceTest.php
+++ tests/Unit/Services/NotificationScheduleServiceTest.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Tests\Integration;
 
+use App\Entity\User;
+use App\Enum\NotificationTime;
 use App\Services\NotificationScheduleService;
 use App\Services\NtfyService;
+use App\Services\UserProviderService;
 use Doctrine\ORM\EntityManager;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
@@ -13,17 +16,27 @@ use PHPUnit\Framework\TestCase;
 final class NotificationScheduleServiceTest extends TestCase
 {
     #[DataProvider('episodeProvider')]
-    public function testFormatNotification(array $episode, string $expectedTitle, string $expectedMessage): void
-    {
+    public function testFormatNotification(
+        array $episode,
+        string $expectedTitle,
+        string $expectedMessage,
+        NotificationTime $notificationTime,
+        int $expectedNotificationTime
+    ): void {
 
         $dummyEm = $this->createStub(EntityManager::class);
         $dummyNtfy = $this->createStub(NtfyService::class);
-        $notificationScheduleService = new NotificationScheduleService($dummyEm, $dummyNtfy);
+        $dummyUserProvider = $this->createStub(UserProviderService::class);
+        $user = new User();
+        $user->setNotificationTime($notificationTime);
+        $dummyUserProvider->method('getById')->willReturn($user);
+        $notificationScheduleService = new NotificationScheduleService($dummyEm, $dummyNtfy, $dummyUserProvider);
 
-        [$title, $message] = $notificationScheduleService->formatNotification($episode);
+        [$title, $message, $timestamp] = $notificationScheduleService->formatNotification($episode);
 
         $this->assertSame($expectedTitle, $title);
         $this->assertSame($expectedMessage, $message);
+        $this->assertSame($expectedNotificationTime, $timestamp);
 
     }
 
@@ -44,19 +57,33 @@ final class NotificationScheduleServiceTest extends TestCase
             'showId' => 1,
             'networkName' => null,
             'webChannelName' => null,
+            'userId' => 1,
             'topics' => ["test1", "test2"]
             ];
 
         return [
-            'title and message' => [$episode, 'test show S1 E2', 'summary'],
+            'title and message' => [
+                $episode,
+                'test show S1 E2',
+                'summary',
+                NotificationTime::AIRTIME,
+                1764775885
+            ],
             'title no SE, summary unavailable' => [
                 array_replace($episode, ['season' => null, 'summary' => null]),
-                'test show', 'Episode summary not available.'],
+                'test show',
+                'Episode summary not available.',
+                NotificationTime::ONE_HOUR_BEFORE,
+                1764772285
+            ],
             'season premiere' => [
                 array_replace($episode, [
                     'season' => 1, 'number' => 1
                 ]),
-                'test show S1 E1', 'test show summary'
+                'test show S1 E1',
+                'test show summary',
+                NotificationTime::ONE_HOUR_AFTER,
+                1764779485
             ],
         ];
     }
