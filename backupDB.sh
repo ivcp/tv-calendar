5e4ed76bb3119b93c3210ad29af08b68f95f4991
Ivo <ivan.perovic@protonmail.com>
2026-02-11 09:11:35 +0100

    remove sudo

--- backupDB.sh
+++ backupDB.sh
@@ -6,14 +6,14 @@ if [ -z "$ORACLE_PAR" ]; then
     exit
 fi
 
-/usr/bin/docker exec -t tv-calendar-db bash -c "pg_dump -U ${DB_USER} -h localhost ${DB_NAME} | gzip > backup.sql.gz"
-/usr/bin/docker cp tv-calendar-db:backup.sql.gz .
-/usr/bin/docker exec -t tv-calendar-db bash -c "rm backup.sql.gz"
+docker exec -t tv-calendar-db bash -c "pg_dump -U ${DB_USER} -h localhost ${DB_NAME} | gzip > backup.sql.gz"
+docker cp tv-calendar-db:backup.sql.gz .
+docker exec -t tv-calendar-db bash -c "rm backup.sql.gz"
 
-/usr/bin/curl -T backup.sql.gz "${ORACLE_PAR}backup.sql.gz"
+curl -T backup.sql.gz "${ORACLE_PAR}backup.sql.gz"
 
 export PATH="/usr/bin:$PATH"
-/usr/bin/mega-put backup.sql.gz /Backups/TvShowCalendar
+mega-put backup.sql.gz /Backups/TvShowCalendar
 
-/usr/bin/rm backup.sql.gz
+rm backup.sql.gz
 
